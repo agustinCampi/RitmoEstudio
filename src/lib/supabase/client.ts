e6e12e37ef2229835d9f279f41436ad5be4c910a
Agustin Campi <agustin.campi@gmail.com>
2025-09-04 15:20:31 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,9 +1,11 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/lib/types/supabase'
 
 export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  return createClientComponentClient<Database>(
+    {
+        supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    }
   )
 }
