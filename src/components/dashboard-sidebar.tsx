d30cf5bade73d640eadbcd7e88e851ad6a73cb92
Agustin Campi <agustin.campi@gmail.com>
2025-09-09 17:52:49 +0000

    basta de errores!
    Console Error
    
    In HTML, <a> cannot be a descendant o

--- src/components/dashboard-sidebar.tsx
+++ src/components/dashboard-sidebar.tsx
@@ -32,10 +32,7 @@ export default function DashboardSidebar() {
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Logo />
-            <span className="">ACME Inc</span>
-          </Link>
+          <Logo />
         </div>
         <div className="flex-1">
           <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
